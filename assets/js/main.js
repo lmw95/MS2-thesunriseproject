699bd49e6b41b55c500f32e0dc17ace07835dc01
Lydia <lmwright95@outlook.com>
2021-07-20 11:04:36 +0000

    Create main.js file in js folder

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+// Function which takes the user to the next section from header - code https://stackoverflow.com/a/8143318
+$("#clickToScroll").click(function(){
+    var position = $("#visitorCenter").position();
+    scroll(0,position.top);
+});
+
+
+
+});
+
