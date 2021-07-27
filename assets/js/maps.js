d2832fac4ad9ecf0ae152e180b9c88e380a4ef8e
Lydia <lmwright95@outlook.com>
2021-07-27 20:30:09 +0000

    Declare all coordinates for map marker variables

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -37,48 +37,32 @@ function initMap() {
 
     // Create all locations for each location's top 5 attractions
     // LOWESTOFT
-    const maritimeMuseum;
-
-    const transportMuseum ;
-
-    const nessPoint;
-
-    const lowestoftBeacH;
-
-    const pleasurewoodHills;
+    const maritimeMuseum = { lat: 52.48786916515505, lng: 1.757501231452841 };
+    const transportMuseum = { lat: 52.45279120791381, lng: 1.6877369984663078 };
+    const nessPoint = { lat: 52.48130428977037, lng: 1.762758745215036 };
+    const lowestoftBeacH = { lat: 52.463528942398, lng: 1.7441735460103207 };
+    const pleasurewoodHills = { lat: 52.508024832271055, lng: 1.7441718423062194 };
 
     //YARMOUTH
-    const pleasureBeach;
-
-    const redwings;
-
-    const raceCourse;
-
-    const yareValley;
-
-    const burghCastle;
+    const pleasureBeach = { lat: 52.595472217209974, lng: 1.735949694382154 };
+    const redwings = { lat: 52.554241310046464, lng: 1.6474202363498287 };
+    const raceCourse = { lat: 52.63021460217459, lng: 1.7318343587904275 };
+    const yareValley = { lat: 52.60317161849138, lng: 1.6820370627771835 };
+    const burghCastle = { lat: 52.583751110675195, lng: 1.6520405685211867 };
     
 
     //KESSINGLAND
-    const africaAlive
-
-    const kessinglandBeach;
-    
-    const benacre;
-
-    const carBoot;
-
-    const suffolkHeathPath;
+    const africaAlive = { lat: 52.413885514052815, lng: 1.7015960432214932 }
+    const kessinglandBeach = { lat: 52.41312737470827, lng: 1.726270528821981 };
+    const benacre = { lat: 52.3836188698611, lng: 1.7015254766199701 };
+    const carBoot = { lat: 52.41577308083052, lng: 1.703570440794497 };
+    const suffolkHeathPath = { lat: 52.40321427281386, lng: 1.7270214872345362 };
 
     //OULTON BROAD
-    const carltonMarshes;
-
-    const nicholasEveritt;
-
-    const riverTours;
-
-    const watersports;
-
-    const commodorePub;
+    const carltonMarshes = { lat: 52.46863102969376, lng: 1.6924630550025137 };
+    const nicholasEveritt = { lat: 52.47432810657495, lng: 1.707210704881762 };
+    const riverTours = { lat: 52.47482176398323, lng: 1.7103077905291226 };
+    const watersports = { lat: 52.472291711185704, lng: 1.7067331167685726 };
+    const commodore = { lat: 52.47621693133902, lng: 1.7109530876879453 };
 
 }
