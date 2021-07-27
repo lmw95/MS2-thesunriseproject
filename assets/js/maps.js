ca11b992513edaf2b40d5e506ceaf02be761e2c3
Lydia <lmwright95@outlook.com>
2021-07-27 18:49:07 +0000

    Create maps.js file and create initMap() to render Google Maps on the page

---
+++ assets/js/maps.js
@@ -0,0 +1,14 @@
+let APIkey = "AIzaSyAz2AgSP-awt4zaXYsWQWIXLev7StjPLLM"; // Google API key
+
+function initMap() {
+    let map = new google.maps.Map(document.getElementById("mapLowestoft"), {
+        zoom: 12,
+        center: {
+            lat: 52.4822390273935,
+            lng: 1.753391400138951
+        }
+    });
+
+
+
+}
