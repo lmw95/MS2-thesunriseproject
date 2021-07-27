84434f80ac34d3d4d607caeb1691e5e4dea2525a
Lydia <lmwright95@outlook.com>
2021-07-27 19:03:42 +0000

    Copy the initMap() for other locations using the relevant coordinates

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,11 +1,35 @@
 let APIkey = "AIzaSyAz2AgSP-awt4zaXYsWQWIXLev7StjPLLM"; // Google API key
 
 function initMap() {
-    let map = new google.maps.Map(document.getElementById("mapLowestoft"), {
-        zoom: 12,
+    let mapL = new google.maps.Map(document.getElementById("mapLowestoft"), {
+        zoom: 13,
         center: {
             lat: 52.4822390273935,
             lng: 1.753391400138951
+        }
+    });
+
+    let mapY = new google.maps.Map(document.getElementById("mapYarmouth"), {
+        zoom: 13,
+        center: {
+            lat: 52.60359934002129, 
+            lng: 1.7262308646134656
+        }
+    });
+
+    let mapK = new google.maps.Map(document.getElementById("mapKessingland"), {
+        zoom: 13,
+        center: {
+            lat: 52.42097755992867,
+            lng: 1.7125956365065302
+        }
+    });
+
+    let mapOB = new google.maps.Map(document.getElementById("mapOultonBroad"), {
+        zoom: 15,
+        center: {
+            lat: 52.47275017240344, 
+            lng: 1.7041162042216889
         }
     });
 
